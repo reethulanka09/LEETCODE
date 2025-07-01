4663a5f436229cbfc873160ec9fe863e66a72288
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-07-01 10:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (60.22%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     int possibleStringCount(string word) {
         int c=1;
-        int i=0,j=i+1;
-        while(j<word.size()){
-            if(word[i]!=word[j]){
-                c+=(j-i-1);
-                i=j;
-            }
-            j++;
+        for(int i=0;i<word.size()-1;i++){
+            if(word[i]==word[i+1]) c++;
         }
-        c+=(j-i-1);
         return c;
     }
 };
