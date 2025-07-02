61dcec42eaa31f17c9f48379d637700786cd8a27
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-07-02 11:39:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (46.41%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.cpp
+++ 1542-consecutive-characters/consecutive-characters.cpp
@@ -3,7 +3,6 @@ public:
     int maxPower(string s) {
         int m=1;
         int i=0,j=0;
-        if(s.size()==2 and s[0]==s[1]) return 2;
         while(j<s.size()){
             if(s[i]!=s[j]){
                 m=max(m,j-i);
