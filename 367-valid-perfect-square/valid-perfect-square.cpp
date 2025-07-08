4d34be13d9ad38941a309b459f232a5c2fcd531f
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-07-08 14:54:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (54.43%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(int(sqrt(num))==sqrt(num)) 
+        {
+            return true;
+        }
+        else{
+            return false;
+        } 
+    }
+};
