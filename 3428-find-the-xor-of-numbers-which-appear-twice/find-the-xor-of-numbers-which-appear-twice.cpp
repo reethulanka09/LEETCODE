e1a9946c5f441a8688f3a572900fbbbe8a907170
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-07-10 14:25:31 +0530

    Time: 8 ms (5.77%) | Memory: 28 MB (27.80%) - LeetSync

--- 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.cpp
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int duplicateNumbersXOR(vector<int>& nums) {
         map<int,int>mpp;
+        int s=0;
         for(int i=0;i<nums.size();i++){
             mpp[nums[i]]++;
-        }
-        int s=0;
-        for(auto i:mpp){
-            if(i.second>1){
-                s^=i.first;
+            if(mpp.find(nums[i])!=mpp.end() and mpp[nums[i]]>1) 
+            {
+                s^=nums[i];
             }
         }
         return s;
