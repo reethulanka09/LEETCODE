4945a25bea68dc03124be866b42725e881062bbc
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-07-14 20:33:31 +0530

    Time: 15 ms (88.67%) | Memory: 63.9 MB (12.43%) - LeetSync

--- 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.cpp
+++ 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int findValueOfPartition(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i=0,j=i+1;
+        sort(nums.begin(),nums.end());int i=0,j=i+1;
         int m=INT_MAX;
-        while(j<nums.size()){
-            m=min(m,abs(nums[i]-nums[j]));
-            i++;
-            j++;
+        for(int i=0;i<nums.size()-1;i++){
+            m=min(m,abs(nums[i]-nums[i+1]));
         }
         return m;
     }
