7a266bb61bf55de3e21492cf715fcd0211e278d2
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-08-09 09:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.81%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n<1){
+            return false;
+        }
+        while(n%2==0){
+            n/=2;
+        }
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
