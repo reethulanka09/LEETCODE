80c17aabc473165eb018a36442bb9857850f42a6
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-08-17 22:12:53 +0530

    Time: 26 ms (88.43%) | Memory: 88.1 MB (60.33%) - LeetSync

--- 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.cpp
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.cpp
@@ -3,14 +3,11 @@ public:
     int maxScore(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         reverse(nums.begin(),nums.end());
-        vector<long long>prefixsum;
-        prefixsum.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-            prefixsum.push_back(prefixsum[i-1]+nums[i]);
-        }
+        long long prefixsum=0;
         int c=0;
-        for(auto i:prefixsum){
-            if(i>0) c++;
+        for(int i=0;i<nums.size();i++){
+            prefixsum+=nums[i];
+            if(prefixsum>0) c++;
         }
         return c;
     }
