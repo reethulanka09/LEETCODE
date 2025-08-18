b7b8a79342c1d9ce2f0c4211d263846d7a93664f
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-08-18 19:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (64.71%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int minMoves(int target, int maxDoubles) {
         long long moves = 0;
-        if(maxDoubles==0) return target-1;
-        while(target>1){
-            if(target%2==0 and maxDoubles>0){
+        // if(maxDoubles==0) return target-1;
+        while(target>1 and maxDoubles>0){
+            if(target%2==0){
                 target /= 2;
                 maxDoubles -=1;
-                moves++;
             }
             else{
                 target -= 1;
-                moves++;
             }
-
+            moves++;
         }
-        return moves;
+        return moves+(target-1);
     }
 };
