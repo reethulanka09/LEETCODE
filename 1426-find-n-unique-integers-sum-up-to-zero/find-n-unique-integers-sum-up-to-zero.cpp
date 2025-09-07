01f85e54751f3ff89884c3d9c321dfa412a43b9f
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-09-07 20:13:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (83.69%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> vec;
+        if (n % 2 != 0) {
+            vec.push_back(0);
+        }
+        for (int i = 1; i <= n/2; i++) {
+            vec.push_back(i);
+            vec.push_back(-i);
+        }
+        return vec;
+    }
+};
