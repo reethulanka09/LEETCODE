7d89fd3d3ee0f08450bc19b4241f03f4eb1685a1
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-09-16 21:00:58 +0530

    Time: 24 ms (44.91%) | Memory: 127.9 MB (39.81%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int gcd(int a, int b) {
-    return (b == 0) ? a : gcd(b, a % b);
-    }
+    // int gcd(int a, int b) {
+    // return (b == 0) ? a : gcd(b, a % b);
+    // }
     int lcm(int a, int b) {
         return (a / gcd(a, b)) * b;
     }
