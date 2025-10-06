cfed4c3f376a5cb8c31f9768a8bcac41ee4cbeca
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-10-06 23:09:27 +0530

    Time: 19 ms (28.84%) | Memory: 14.4 MB (14.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,12 +6,10 @@ public:
 
     while (right < s.length()) {
         if (seen.find(s[right]) == seen.end()) {
-            // Character not seen, add to set and move right pointer
             seen.insert(s[right]);
             maxLen = max(maxLen, right - left + 1);
             right++;
-        } else {
-            // Duplicate found, remove the leftmost character and move left
+        } else { 
             seen.erase(s[left]);
             left++;
         }
