99e5ce6713bdaf18c9914943355b5a82c43297c9
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-10-17 20:30:03 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (55.62%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<vector<int>>ans;
+        if(root==NULL) return ans;
+        while(!q.empty()){
+            vector<int>res;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+
+            TreeNode* node = q.front();
+            q.pop();
+            if(node->left!=NULL) q.push(node->left);
+            if(node->right!=NULL) q.push(node->right);
+            res.push_back(node->val);
+            }
+        ans.push_back(res);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
