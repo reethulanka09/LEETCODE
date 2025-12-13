22f1003c974afdb52128b82ad6e92232c76e28ca
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-12-13 14:06:08 +0530

    Time: 15 ms (13.30%) | Memory: 69.2 MB (60.51%) - LeetSync

--- 3934-coupon-code-validator/coupon-code-validator.cpp
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -2,34 +2,31 @@ class Solution {
 public:
     vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
         int n = code.size();
-        unordered_map<string, int> businessLineSortOrder = {
-            {"electronics", 0},
-            {"grocery", 1},
-            {"pharmacy", 2},
-            {"restaurant", 3}
-        };
-        vector<pair<pair<int, string>, string>> sortableCoupons;
-        for (int i = 0; i < n; ++i) {
-            if (!isActive[i]) continue;
-            if (businessLineSortOrder.find(businessLine[i]) == businessLineSortOrder.end()) continue;
-            if (code[i].empty()) continue; 
-            bool isCodeValid = true;
-            for (char c : code[i]) {
-                if (!(isalnum(c) || c == '_')) {
-                    isCodeValid = false;
+        vector<string> ans;
+        vector<string> categ={"electronics", "grocery", "pharmacy", "restaurant"};
+        unordered_map<string,vector<string>> mp;
+        for(int i=0;i<n;i++){
+            if(!isActive[i])continue;
+            if(businessLine[i]!=categ[0]&&businessLine[i]!=categ[1]&&businessLine[i]!=categ[2]&&businessLine[i]!=categ[3])continue;
+            int f=1;
+            if(code[i].size()==0)continue;
+            for(int j=0;j<code[i].size();j++){
+                if(!((code[i][j]=='_' ) || (code[i][j]-'a'<26&&code[i][j]-'a'>=0) || (code[i][j]-'A'<26&&code[i][j]-'A'>=0) || (code[i][j]-'0'<=9&&code[i][j]-'0'>=0))){
+                    f=-1;
                     break;
                 }
             }
-            if (!isCodeValid) continue;
-            int sortIndex = businessLineSortOrder[businessLine[i]];
-            sortableCoupons.push_back({{sortIndex, code[i]}, code[i]});
+            if(f==-1)continue;
+            mp[businessLine[i]].push_back(code[i]);
         }
-        sort(sortableCoupons.begin(), sortableCoupons.end());
-        vector<string> sortedValidCodes;
-        for (auto& entry : sortableCoupons) {
-            sortedValidCodes.push_back(entry.second);
+        for(int i=0;i<4;i++){
+            auto it = mp.find(categ[i]);
+            if(it == mp.end())continue;
+            sort(it->second.begin(),it->second.end());
+            for(int j=0;j<it->second.size();j++){
+                ans.push_back(it->second[j]);
+            }
         }
-
-        return sortedValidCodes;
+        return ans;
     }
 };
