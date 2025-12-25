5ff93bcf77397e4ed9d892cae9c6f5c69d311f26
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2025-12-25 14:36:05 +0530

    Time: 160 ms (49.65%) | Memory: 107.9 MB (65.96%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -4,14 +4,11 @@ public:
         int r = k;
         long long ans =0;
         sort(happiness.begin(),happiness.end());
-        for(int i=happiness.size()-1;i>=0;i--){
-            if(k>0 and happiness[i]-(r-k)>0){
-            ans+=happiness[i]-(r-k);
-            k--;
-            }
-            else {
-                break;
-            }
+        reverse(happiness.begin(),happiness.end());
+        int i=0;
+        while(k--){
+            happiness[i] = max(happiness[i]-i,0);
+            ans+=happiness[i++];
         }
         return ans;
     }
