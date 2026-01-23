3077dc289ddacba85ad675b59824fdc816ba2a67
PRIYA REETHIKA LANKA <119839740+reethulanka09@users.noreply.github.com>
2026-01-23 19:58:57 +0530

    Time: 18 ms (99.24%) | Memory: 233.6 MB (99.55%) - LeetSync

--- 4152-minimum-subarray-length-with-distinct-sum-at-least-k/minimum-subarray-length-with-distinct-sum-at-least-k.cpp
+++ 4152-minimum-subarray-length-with-distinct-sum-at-least-k/minimum-subarray-length-with-distinct-sum-at-least-k.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-        int minLength(vector<int>& A, int k) {
-        unordered_map<int, int> cnt;
-        int i = 0, n = A.size(), res = n + 1;
-        for (int j = 0; j < A.size(); ++j) {
-            int a = A[j];
-            if (++cnt[a] == 1)
-                k -= a;
-            while (k <= 0) {
-                res = min(res, j - i + 1);
-                if (--cnt[A[i]] == 0)
-                    k += A[i];
+    int minLength(vector<int>& nums, int k) {
+        if(nums.size()==1 && nums[0]>=k) return 1;
+        int i=0,j=0, ans=100001;
+        int sum=0;
+        int mp[100001]={0};
+        while(j<nums.size()){
+            mp[nums[j]]++;
+            if(mp[nums[j]]==1){
+                sum+=nums[j];
+            }
+            while(sum>=k){
+                if(mp[nums[i]]==1){
+                    sum-=nums[i];
+                }
+                mp[nums[i]]--;
+                ans=min(ans, j-i+1);
                 i++;
             }
+            j++;
         }
-        return res > n ? -1 : res;
+        if(ans==100001) return -1;
+        return ans;
     }
 };
