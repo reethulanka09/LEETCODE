325894ac0449b28bd1153e8ba49dd41a1b77d8da
PRIYA REETHIKA LANKA <22A91A05I8@aec.edu.in>
2026-05-19 15:51:00 +0530

    Time: 6 ms (66.97%) | Memory: 11.8 MB (66.11%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> seen;
-    int left = 0, right = 0, maxLen = 0;
-
-    while (right < s.length()) {
-        if (seen.find(s[right]) == seen.end()) {
-            seen.insert(s[right]);
-            maxLen = max(maxLen, right - left + 1);
-            right++;
-        } else { 
-            seen.erase(s[left]);
-            left++;
-        }
+       unordered_map<char,int>mpp;
+       int ans =0;
+       int i=0,j=0;
+       while(j<s.size()){
+            mpp[s[j]]++;
+            if(mpp[s[j]]>1){
+                ans=max(ans,j-i);
+                while(mpp[s[j]]>1){
+                    mpp[s[i]]--;
+                    i++;
+                }
+            }
+            j++;
+       } 
+       ans=max(ans,j-i);
+       return ans;
     }
-
-    return maxLen;
-}
 };
