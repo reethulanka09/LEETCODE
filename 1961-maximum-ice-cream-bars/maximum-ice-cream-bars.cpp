180acf62d735c6d176d2fea86afe6dd5ba63bc53
PRIYA REETHIKA LANKA <22A91A05I8@aec.edu.in>
2026-06-21 12:16:04 +0530

    Time: 8 ms (86.81%) | Memory: 84.4 MB (19.23%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int ans=0;
-        for(int i=0;i<costs.size();i++){
-        if(costs[i]<=coins and coins>0)
-        {
-            ans++;
-            coins-=costs[i];
-        }
+        int ans =0;
+        int mx = *max_element(costs.begin(),costs.end());
+        vector<int>counter(mx+1,0);
+        for(auto count:costs) counter[count]++;
+        for(int i=1;i<=mx;i++){
+            while(counter[i]>0 && coins>=i){
+                counter[i]--;
+                ans++;
+                coins-=i;
+
+            }
         }
         return ans;
     }
