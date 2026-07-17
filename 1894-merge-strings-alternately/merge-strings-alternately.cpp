dca909d440f353ef63bf6ec34b1f3a94de9ba7d9
PRIYA REETHIKA LANKA <22A91A05I8@aec.edu.in>
2026-07-17 10:05:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.56%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans;
+        int i=0,j=0;
+        while(i< word1.size() && j < word2.size()){
+            ans+=word1[i];
+            ans+=word2[j];
+            i++;
+            j++;
+        }
+        if(word1.size() > word2.size()){
+            for(int k=i;k<word1.size();k++){
+                ans+=word1[k];
+            }
+        }
+        else{
+            for(int k=j;k<word2.size();k++){
+                ans+=word2[k];
+            }
+        }
+        return ans;
+    }
+};
