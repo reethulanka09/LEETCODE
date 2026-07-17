808d667bab87bf11c68148e9896583552b047358
PRIYA REETHIKA LANKA <22A91A05I8@aec.edu.in>
2026-07-17 12:01:31 +0530

    Time: 1 ms (15.64%) | Memory: 24.1 MB (45.06%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        if(flowerbed.size() ==1){
-            if(flowerbed[0]==1 and n==1){
-                return false;
+        for(int i=0;i<flowerbed.size();i++){
+            if(flowerbed[i]==0){
+                bool checkLeftSLot = (i==0) || flowerbed[i-1] == 0;
+                bool checkRightSLot = (i==flowerbed.size()-1) || flowerbed[i+1] == 0;
+                if(checkLeftSLot && checkRightSLot){
+                    n--;
+                    flowerbed[i]=1;
+                }
             }
-            else{
-                return true;
-            }
-        }
-        if(flowerbed.size() > 1 and flowerbed[0]==0 and flowerbed[1]!=1){
-            n--;
-             flowerbed[0]=1;
-        } 
-        for(int i=1;i<flowerbed.size();i++){
-           if(i==flowerbed.size()-1){
-            if(flowerbed[i]==0 and flowerbed[i-1]!=1){
-                n--;
-                flowerbed[i]=1;
-            }
-           }
-           else{
-              if(flowerbed[i]==0 and flowerbed[i-1] != 1 and flowerbed[i+1] != 1){
-                n--;
-                flowerbed[i]=1;
-              }
-           }
         }
         if(n>0) return false;
-        else return true;
+        return true;
     }
 };
