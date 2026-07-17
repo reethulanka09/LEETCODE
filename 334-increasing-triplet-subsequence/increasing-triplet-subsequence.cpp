f8d086d29ee503123ba287fa320666eede8908cc
PRIYA REETHIKA LANKA <22A91A05I8@aec.edu.in>
2026-07-17 16:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 115.8 MB (12.47%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        bool flag = false;
-        int len = nums.size();
         int min1 = INT_MAX;
         int min2 = INT_MAX;
-        for(int i =0 ; i <len;i++ ){
-            if(nums[i]<=min1){
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<= min1){
                 min1=nums[i];
-                
             }
-            else if (nums[i]<=min2){
-                min2 = nums[i];
-            }else{
-                flag =  true;
+            else if(nums[i]<=min2){
+                min2=nums[i];
+            }
+            else {
+                return true;
             }
         }
-        return flag;
+        return false;
     }
 };
