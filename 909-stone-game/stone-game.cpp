784c97ea0e9ca97fc5867e609229d94181bcf862
PRIYA REETHIKA LANKA <22A91A05I8@aec.edu.in>
2026-08-02 21:40:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (68.24%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,32 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& nums) {
-        int i=0,j=nums.size()-1;
-        int s=0;
-        for(auto i:nums){
-            s+=i;
-        }
-        int alicesum = 0;
-        while(i<j){
-            if(nums[i]>nums[j]){
-                alicesum+=nums[i];
-                i++;
-            }
-            else if(nums[j]>nums[i]){
-                alicesum+=nums[j];
-                j--;
-            }
-            else{
-                alicesum+=nums[i];
-                i++;
-                j--;
-            }
-        }
-        if(alicesum > s-alicesum){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
 };
