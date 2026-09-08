2d08a63549a159adc202a0c8f195a729f664076a
PRIYA REETHIKA LANKA <22A91A05I8@aec.edu.in>
2026-09-08 14:06:05 +0530

    Time: 3 ms (8.65%) | Memory: 19.4 MB (65.32%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int ind1 = 0 , ind2 = 0;
         int i = 0, j = numbers.size() - 1;
         while(i < j){
             if(numbers[i] + numbers[j] == target){
-                ind1 = i+1;
-                ind2 = j+1;
-                break;
+                return {i+1,j+1};
             }
             else if(numbers[i] + numbers[j] > target){
                 j--;
@@ -16,6 +13,6 @@ public:
                 i++;
             }
         }
-        return {ind1,ind2};
+        return {};
     }
 };
