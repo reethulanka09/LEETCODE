0e927d4dbe328b992418217671e6d3d54a1eb678
PRIYA REETHIKA LANKA <22A91A05I8@aec.edu.in>
2026-09-08 13:40:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (77.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if(s == " ") return true;
         int i = 0, j = s.size() - 1;
         while(i < j){
             while(i < j  and !isalnum(s[i])){
